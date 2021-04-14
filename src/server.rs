34c30f801d35c98ad20509c6a6322de19f7eec4a
dianestephens <diane@mljsoftware.com>
2021-04-13 19:16:27 -0700

    added code to perform search -f file -s search text; remaining search -s search text, sending response message to client, error handline

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,12 @@
 // server to listen for commands from client threads
+use std::env;
+mod search;
+use std::fs;
+use std::io::prelude::*;
+use std::path::PathBuf;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+use std::ops::{Bound, RangeBounds};
 
 use std::io::{Read, Write, Error};
 
@@ -8,25 +14,71 @@ use std::io::{Read, Write, Error};
 fn connection_thread(mut stream: TcpStream) -> Result<(), Error> {
 	println!("Received a connection from: {}", 
 		stream.peer_addr()?);	// network address of client unwrap to OK if
-	let mut buffer = [0; 256]; 	// zero buffer
+	let mut buffer = [0; 1024]; 	// zero buffer
 	loop {
 		let bytes_read = stream.read(&mut buffer)?; // unwrap to Ok if read from stream successful
 		if bytes_read == 0 {	// no more to read
 			return Ok(());
+			
 		}
 		
-		let cmd_line = String::from_utf8_lossy(&buffer[..]);
-		println!("cmd_line from buffer: {}", cmd_line);
+		let cmd_line = String::from_utf8_lossy(&buffer[0..bytes_read]);
+		//println!("cmd_line from buffer: {}", cmd_line);
 		
 		// split the command line into tokens: tokens[0] .. tokens[params-1]
 		
 		let tokens: Vec<&str> = cmd_line.trim().split_whitespace().collect();
-    		
+    		let mut args: Vec<String> = cmd_line.trim().split_whitespace().map(str::to_string).collect();
     		
 		match tokens[0] {
 			"search" => {
 				println!("SEARCH HERE !!!");
 				//assert_eq!(tokens.len(), 4);
+				let mut path = PathBuf::new();
+    				path.push("./users_client/");
+    				path.push("diane/");
+    				let file_name;
+        			let search_text;
+        			if args[1] == "-f" && args.iter().any(|i| i=="-s"){
+        				println!("f and s found\n");
+            				if args.len() < 5 {
+                				println!("Please insert the correct format and try again! The format is “search -f [FILE NAME] -s [TEXT]” or “search -f [FILE NAME] -s [TEXT]”");
+                				//break;
+            				}
+            				file_name = &args[2];
+            				path.push(file_name);
+					
+            				let search_text_args: Vec<_> = args.drain(4..).collect();
+            			
+            				search_text = search_text_args.join(" ");
+            				
+            				
+            			// fix this error handling of file open	
+            			//	let file = fs::File::open(path);
+            			 	let mut file = fs::File::open(path).expect("Can't open file!");
+            				
+            				let mut contents = String::new();
+
+             				
+            			/*	let mut file=match file {
+                				Ok(file) => file,
+                				Err(error) => {
+                    					eprintln!("Problem opening the file: {}",error);
+                    					break
+                				}
+             				};
+				*/
+            				file.read_to_string(&mut contents).map_err(
+                				|err| println!("{:?}", err)
+            
+            					).ok();
+            				
+            				search::search_f(&contents, &search_text);
+            				
+            				//break;
+             	
+            			}
+        			
 			},
 			
 			_ => 
@@ -62,4 +114,49 @@ fn main() {
 			}
 		}
 	}
+}
+
+
+trait StringUtils {
+    fn substring(&self, start: usize, len: usize) -> &str;
+    fn slice(&self, range: impl RangeBounds<usize>) -> &str;
+}
+
+impl StringUtils for str {
+    fn substring(&self, start: usize, len: usize) -> &str {
+        let mut char_pos = 0;
+        let mut byte_start = 0;
+        let mut it = self.chars();
+        loop {
+            if char_pos == start { break; }
+            if let Some(c) = it.next() {
+                char_pos += 1;
+                byte_start += c.len_utf8();
+            }
+            else { break; }
+        }
+        char_pos = 0;
+        let mut byte_end = byte_start;
+        loop {
+            if char_pos == len { break; }
+            if let Some(c) = it.next() {
+                char_pos += 1;
+                byte_end += c.len_utf8();
+            }
+            else { break; }
+        }
+        &self[byte_start..byte_end]
+    }
+    fn slice(&self, range: impl RangeBounds<usize>) -> &str {
+        let start = match range.start_bound() {
+            Bound::Included(bound) | Bound::Excluded(bound) => *bound,
+            Bound::Unbounded => 0,
+        };
+        let len = match range.end_bound() {
+            Bound::Included(bound) => *bound + 1,
+            Bound::Excluded(bound) => *bound,
+            Bound::Unbounded => self.len(),
+        } - start;
+        self.substring(start, len)
+    }
 }
